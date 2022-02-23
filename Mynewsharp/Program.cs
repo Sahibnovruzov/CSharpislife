06321878e09962cb36590603a15b9e186001cd81
Sahib <sahib.novruzov1996@outlook.com>
2022-02-23 17:30:41 +0400

    first commit

--- Mynewsharp/Program.cs
+++ Mynewsharp/Program.cs
@@ -1,2 +1,107 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyApp
+{
+    public class Program
+    {
+       public static void Main(string[] args)
+        {
+            
+                DateTime dateTime = DateTime.UtcNow;
+            //Console.WriteLine((int)dateTime.DayOfYear);
+            // Console.WriteLine(dateTime.AddYears(222));\
+           // Console.WriteLine(dateTime.Add(TimeSpan.Parse)("30.25.10"));
+
+
+        }
+
+
+
+
+    }
+}
+            
+            //int a = 5;
+            //int b = 5;
+            // int sum = a + b;
+            //char a = 'a';
+            //char b = 'b';
+            //var var1 = char.IsNumber(a);
+            // var var2 = char.IsLetter(b);
+            //string a = "asdsdswd";
+            //string b = a.ToUpper();
+            //double a = Math.PI;
+            //int a = 5;
+            //string b = a.ToString();
+            //int a = 6;
+            // int b = 7;
+            // Console.WriteLine(a+b);
+            //  Console.WriteLine(++a);
+            //expicit inversion
+            //b = (short)c;
+            // Console.WriteLine(a);
+            //int a = 7;
+            /*  int a = 5;
+              int b = 7;
+              if (a %2 ==0 && b%2==0 )
+              {
+                  Console.WriteLine("cut");
+              }
+              else
+              {
+                  Console.WriteLine("tek");
+              }
+            */
+
+            //string a = "Azerbaycan";
+           // string b = "can";
+           // Console.WriteLine(a.Contains(b));
+           /*
+            DateTime dateTime = DateTime.Now;
+
+            Console.WriteLine(dateTime.DayOfWeek);
+
+            Console.ReadLine();
+        }
+           */
+        
+            /*
+
+           var student = (MyEnum)int.Parse(Console.ReadLine());
+
+            if (student==MyEnum.BadStudent)
+            {
+                Console.WriteLine("bad");
+            }
+            else if (student == MyEnum.GoodStudent)
+            {
+                Console.WriteLine("good");
+            }
+            else if (student==MyEnum.ExcellentStudent)
+            {
+                Console.WriteLine("excellent");
+            }
+         
+
+
+
+
+
+            Console.ReadLine();
+        }
+        
+        
+        
+        public enum MyEnum
+        {
+            BadStudent=5,
+            GoodStudent=7,
+            ExcellentStudent=9,
+            */
+        
